852029f3d559bde5bad3335721383e13ff173e2b
Vinícius Assis <viniciusassisilva@gmail.com>
2024-03-03 13:27:53 -0300

    1008-1012

---
+++ BeeCrowd1011.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BeeCrowd1011 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner s = new Scanner(System.in);
+
+        double pi = 3.14159;
+        double raio = s.nextDouble();
+
+        double volume = (4.0/3) * pi * raio*raio*raio;
+
+        System.out.printf("VOLUME = %.3f%n", volume);
+    }
+}
